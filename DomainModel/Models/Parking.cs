66bb328ee417721450091be6267dcfc9485b2e38
Fani Tafra <fanit6@gmail.com>
2023-03-20 23:42:21 +0100

    ModelParking

--- DomainModel/Models/Parking.cs
+++ DomainModel/Models/Parking.cs
@@ -8,5 +8,15 @@ namespace DomainModel.Models
 {
     public class Parking
     {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Neighborhood { get; set; }
+        public string OwnerId { get; set; }
+        public string UserId { get; set; }
+        public DateTime ParkingFrom { get; set; }
+        public DateTime ParkingTo { get; set; }
+        public ParkingStatus Status { get; set; }
+        public User? User { get; set; }
+
     }
 }
