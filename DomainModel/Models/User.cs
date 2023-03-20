3745894849ca938fd0045a67aaaadaf724d25ca3
Fani Tafra <fanit6@gmail.com>
2023-03-20 23:44:24 +0100

    ModelUser

--- DomainModel/Models/User.cs
+++ DomainModel/Models/User.cs
@@ -8,5 +8,14 @@ namespace DomainModel.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public DateOnly Birthday { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
