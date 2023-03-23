f2e7b6225a99331f0957a50c437939a95f1cc5ac
Fani Tafra <fanit6@gmail.com>
2023-03-23 20:38:37 +0100

    changed dateOnly to DateTime

--- DomainModel/Models/User.cs
+++ DomainModel/Models/User.cs
@@ -14,7 +14,7 @@ namespace DomainModel.Models
         public string LastName { get; set; }
         public string Password { get; set; }
         public string Gender { get; set; }
-        public DateOnly Birthday { get; set; }
+        public DateTime Birthday { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
     }
