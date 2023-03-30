ec2260f7dac651cc64b74134573070e942b8d92b
Fani Tafra <fanit6@gmail.com>
2023-03-30 19:40:52 +0200

    repository_parking

---
+++ ParkingManagementAPI/Repository/ParkingRepository.cs
@@ -0,0 +1,17 @@
+﻿using ParkingManagementAPI.DatabaseContext;
+
+namespace ParkingManagementAPI.Repository
+{
+    public class ParkingRepository
+    {
+        private readonly ParkingDbContext _parkingDbContext;
+        public ParkingRepository(ParkingDbContext parkingDbContext)
+        {
+            _parkingDbContext = parkingDbContext;
+        }
+        public IEnumerable<Parking> GetParkings()
+        {
+            return _parkingDbContext.Parkings.ToList();
+        }
+    }
+}
