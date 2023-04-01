29a86d03a432b04c6c1917e53e9e67fef9ea6a79
Fani Tafra <fanit6@gmail.com>
2023-04-01 18:23:45 +0200

    blazor list and service

---
+++ ParkingManagementBlazorServer/Services/ParkingService.cs
@@ -0,0 +1,18 @@
+﻿using DomainModel.Models;
+
+namespace ParkingManagementBlazorServer.Services
+{
+    public class ParkingService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string BaseApiUrl = "http://localhost:7289";
+        public ParkingService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<Parking>> GetParkings()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Parking>>(BaseApiUrl);
+        }
+    }
+}
