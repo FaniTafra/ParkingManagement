a3262e93d430bcf713eca30016289adf0e64a5b4
Fani Tafra <fanit6@gmail.com>
2023-04-02 16:54:04 +0200

    frontend

--- ParkingManagementBlazorServer/Program.cs
+++ ParkingManagementBlazorServer/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ParkingManagementBlazorServer.Data;
+using ParkingManagementBlazorServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient();
+builder.Services.AddScoped<ParkingService>();
 
 var app = builder.Build();
 
--- ParkingManagementBlazorServer/Services/ParkingService.cs
+++ ParkingManagementBlazorServer/Services/ParkingService.cs
@@ -1,11 +1,13 @@
 ﻿using DomainModel.Models;
+using System.Text.Json;
+using System.Text;
 
 namespace ParkingManagementBlazorServer.Services
 {
     public class ParkingService
     {
         private readonly HttpClient _httpClient;
-        private readonly string BaseApiUrl = "http://localhost:7289";
+        private readonly string BaseApiUrl = "https://localhost:7289/api/Parking";
         public ParkingService(HttpClient httpClient)
         {
             _httpClient = httpClient;
@@ -13,6 +15,13 @@ namespace ParkingManagementBlazorServer.Services
         public async Task<List<Parking>> GetParkings()
         {
             return await _httpClient.GetFromJsonAsync<List<Parking>>(BaseApiUrl);
+        }
+        public async Task AddParkingAsync(Parking parking)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, BaseApiUrl);
+            request.Content = new StringContent(JsonSerializer.Serialize(parking), Encoding.UTF8,
+            "application/json");
+            await _httpClient.SendAsync(request);
         }
     }
 }
