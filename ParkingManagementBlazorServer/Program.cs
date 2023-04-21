259753b2ecdd5a0721e6dfcc56df8e8391b8c3b5
Fani Tafra <fanit6@gmail.com>
2023-04-21 21:13:17 +0200

    edit pop up

--- ParkingManagementBlazorServer/Program.cs
+++ ParkingManagementBlazorServer/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using ParkingManagementBlazorServer.Data;
 using ParkingManagementBlazorServer.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddHttpClient();
 builder.Services.AddScoped<ParkingService>();
 //builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(7289));
