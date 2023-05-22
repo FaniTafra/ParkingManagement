c377adc35561b57858ff55974a908fc5510311bd
Fani Tafra <fanit6@gmail.com>
2023-05-22 17:40:47 +0200

    Added User

---
+++ ParkingManagementBlazorServer/Services/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+using DomainModel.Models;
+
+
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string BaseApiUrl = "https://localhost:7289/api/User";
+        public UserService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, BaseApiUrl);
+            request.Content = new StringContent(JsonSerializer.Serialize(user), Encoding.UTF8, "application/json");
+            await _httpClient.SendAsync(request);
+        }
+    }
+
